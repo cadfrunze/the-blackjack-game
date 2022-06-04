dfb5ca09fa6c4e1f39ae361d671bf2e448c2fe96
cadfrunze <78301491+cadfrunze@users.noreply.github.com>
2022-06-04 13:57:45 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,20 @@
+import random
+
+dict_game = {
+    "A": [1, 11],
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "10": 10,
+    "J": 10,
+    "Q": 10,
+    "K":10
+}
+
+card = random.choice(list(dict_game.items()))
+print(type(card))
