62ec5291bdb4e864610acbc306d4f7bbeef1de67
cadfrunze <78301491+cadfrunze@users.noreply.github.com>
2022-06-07 00:22:43 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,20 +1,21 @@
-import random
+# dict_symb = {
+#         "A": [1, 11],
+#         "2": 2,
+#         "3": 3,
+#         "4": 4,
+#         "5": 5,
+#         "6": 6,
+#         "7": 7,
+#         "8": 8,
+#         "9": 9,
+#         "10": 10,
+#         "J": 10,
+#         "Q": 10,
+#         "K": 10,
 
-dict_game = {
-    "A": [1, 11],
-    "2": 2,
-    "3": 3,
-    "4": 4,
-    "5": 5,
-    "6": 6,
-    "7": 7,
-    "8": 8,
-    "9": 9,
-    "10": 10,
-    "J": 10,
-    "Q": 10,
-    "K":10
-}
+#     }
+# test1 = dict_symb["A"][1] + 1
+# print(test1)
 
-card = random.choice(list(dict_game.items()))
-print(type(card))
+for a in range(0, 3):
+    print(a)
