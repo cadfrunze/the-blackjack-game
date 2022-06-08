796506cbb81b26a3acb142d2cd13129ce1669908
cadfrunze <78301491+cadfrunze@users.noreply.github.com>
2022-06-08 14:44:00 +0300

    Update test.py

--- test.py
+++ test.py
@@ -17,5 +17,17 @@
 # test1 = dict_symb["A"][1] + 1
 # print(test1)
 
-for a in range(0, 3):
-    print(a)
+# for a in range(0, 3):
+#     print(a)
+import time
+import os
+from art import logo
+logo1 = ""
+for afisare in range(len(logo)):
+    logo1 = logo1 + logo[afisare]
+    if afisare < 7:
+        time.sleep(1)
+    elif afisare > 7  and afisare < 18:
+        time.sleep(0.3)
+    os.system('cls')
+    print(logo1)
