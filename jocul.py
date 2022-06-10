d73fea640fd44b7d2c4a631f8d752a7839d82d04
cadfrunze <78301491+cadfrunze@users.noreply.github.com>
2022-06-10 21:22:29 +0300

    Revert "Update jocul.py"
    This reverts commit 870e795327d49e25d05f430245d9db433684914c.

--- jocul.py
+++ jocul.py
@@ -10,9 +10,9 @@ userii = {
 }
 scor_1 = 0
 scor_2 = 0
-def jocul(innregistrare, userii, scor_1, scor_2,scorul):
-    """The black-jack game!"""
-    # Innregistrare useri (prima data cand ruleaza jocul)
+def jocul(innregistrare, userii, scor_1, scor_2):
+    """Jocul blackjack"""
+    
     if innregistrare == True:
         user1 = input("Username >>> ").capitalize()
         os.system('cls')
@@ -27,7 +27,7 @@ def jocul(innregistrare, userii, scor_1, scor_2,scorul):
         time.sleep(2)
         print("1. Cartile pentru joc sunt: [*'A', '2', '3', '4', '5', '6', '7', '8', '9', '10', *'A', 'J', 'Q', 'K'].")
         time.sleep(2)
-        print("2. Fiecare carte reprezinta un numar, (*cu exceptia cartii 'A' care poate fi 1 sau 11).\n   2.1. Astfel:\n       2.1.1 Cartea 'J' este egala 10,\n       2.1.2 Cartea 'Q' este egala 10,\n       2.1.3 Cartea 'K' este egala tot cu 10.")
+        print("2. Fiecare carte reprezinta un numar, (*cu exceptia cartii 'A' care poate fi 1 sau 11).\n   2.1 Astfel:\n       cartea 'J' este egala 10,\n       cartea 'Q' este egala 10,\n       cartea 'K' este egala tot cu 10.")
         time.sleep(2)
         print(f"3. {user1} primadata vei primi doua carti, si vor arata de exemplu asa: ['2', '7'].")
         time.sleep(2)
@@ -35,7 +35,7 @@ def jocul(innregistrare, userii, scor_1, scor_2,scorul):
         time.sleep(2)
         print("5. Daca suma celor doua carti de exemplu: ['7', '3'] (7 + 3 = 10) este mai mica sau egala cu 10, atunci cartea 'A' este egala cu 11.\n   5.1. Pentru ca nu-i asa? 10 + 'A'(adica 11) = 21 :).\n   5.2. Iar daca suma numerelor din tabel de exemplu ['J', '5'] adica (15) depaseste suma de (11) atunci cartea 'A' este egala cu 1.) ")
         time.sleep(2)
-        print("6. Intotdeauna cand ceri inca o carte dealerului ultima carte ti se va afisa in acest tabel [....].\n   6.1. De exemplu ['7', '3', 'A'] ultima carte fiind 'A'.\n   6.2. Cartea ti se va genera absolut aleatoriu (random) din tabelul de la pct 1.")
+        print("6. Intotdeauna cand ceri inca o carte dealerului ultima carte ti se va afisa in acest tabel [....].\n   6.1. De exemplu ['7', '3', 'A'] ultima carte fiind 'A'.\n   6.2. Cartea ti se va genera absolut aleatoriu (random) din tabelul de la pct1.")
         time.sleep(2)
         print("7. Scopul acestui joc este cine dintre playeri face 21, sau cine se apropie cel mai mult de numarul 21.\n   7.1. Cine face direct 21 din totalul cartilor automat va castiga partida!(fara a mai juca dealerul).\n   7.2. Tine minte! Dealerul este intotdeauna playerul calculator cel cu (\"-Both\").")
         time.sleep(2)
@@ -64,22 +64,21 @@ def jocul(innregistrare, userii, scor_1, scor_2,scorul):
                 sys.exit()
             acord = input("Raspunde aici >>> ")
             incercare = incercare + 1
-        os.system('cls')
-        print("Al doilea player (calculator) este " + userii["user2"])   
-        time.sleep(2)
-        os.system('cls')
-        print("Loading the game....")
-        time.sleep(3)
-        logo1 = ""
-        for afisare in range(len(logo)):
-            logo1 = logo1 + logo[afisare]
-            if afisare < 7:
-                time.sleep(1)
-            elif afisare > 7  and afisare < 18:
-                time.sleep(0.3)
             os.system('cls')
-            print(logo1)
-        time.sleep(3)
+            print("Al doilea player (calculator) este " + userii["user2"])   
+    os.system('cls')
+    print("Loading the game....")
+    time.sleep(3)
+    logo1 = ""
+    for afisare in range(len(logo)):
+        logo1 = logo1 + logo[afisare]
+        if afisare < 7:
+            time.sleep(1)
+        elif afisare > 7  and afisare < 18:
+            time.sleep(0.3)
+        os.system('cls')
+        print(logo1)
+    time.sleep(3)
     symboluri = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
     dict_symb = {
         "A": [1, 11],
@@ -103,7 +102,7 @@ def jocul(innregistrare, userii, scor_1, scor_2,scorul):
     user_2_int = 0
     game1 = True
     game2 = True
-    # Rularea jocului
+    
     for numar in range(0, 3):
         if numar == 0:
             user1_prov = random.choice(symboluri)
@@ -148,13 +147,7 @@ def jocul(innregistrare, userii, scor_1, scor_2,scorul):
                     user_1_int.append(dict_symb["A"][0])
             else:
                 user_1_int.append(user1_prov)
-    # Rulare joc player 1
     while game1:
-        if scorul == True:
-            print("Pana acum scorul este:")
-            print(rezultat["user1"][0] + ": " + str(rezultat["user1"][3]) + ".")
-            print(rezultat["user2"][0] + ": " + str(rezultat["user2"][3]) + ".")
-
         if sum(user_1_int) <= 20 or sum(user_1_int) == 21:
             if sum(user_1_int) <= 10:
                 os.system('cls')
@@ -231,13 +224,7 @@ def jocul(innregistrare, userii, scor_1, scor_2,scorul):
             while x != "":
                 x = input("Apasa enter pt a continua! (fara a scrie nimic in consola!)")
             print("Acum este randul " + userii["user2"] + " sa joace")
-    # Rulare joc player 2
     while game2:
-        if scorul == True:
-            print("Pana acum scorul este:")
-            print(rezultat["user1"][0] + ": " + str(rezultat["user1"][3]) + ".")
-            print(rezultat["user2"][0] + ": " + str(rezultat["user2"][3]) + ".")
-
         if user_2_int <= 17:
             os.system('cls')
             print(logo)
@@ -266,118 +253,91 @@ def jocul(innregistrare, userii, scor_1, scor_2,scorul):
                 print(logo)
                 print("Wow " + userii["user2"] + f" cartile lui sunt {user_2_str} si a facut {user_2_int} Yuhuuuu!!!")
                 scor_2 = scor_2 + 1
-                x = input("Apasa enter pt a continua!")
-                while x != "":
-                    x = input("Apasa enter pt a continua! (fara a scrie nimic in consola!)")
             else:
                 os.system('cls')
                 print(logo)
                 print(userii["user2"] + f" cartile lui sunt {user_2_str} si punctajul sau este {user_2_int}, a renuntat sa mai joace!")
-                x = input("Apasa enter pt a continua!")
-                while x != "":
-                    x = input("Apasa enter pt a continua! (fara a scrie nimic in consola!)")
+                game2 = False
 
         elif user_2_int > 21:
             os.system('cls')
             print(logo)
             print("Din pacate " + userii["user2"] + f" cartile lui sunt {user_2_str} a facut {user_2_int} punctajul lui este peste 21")
             game2 = False
-            x = input("Apasa enter pt a continua!")
-            while x != "":
-                x = input("Apasa enter pt a continua! (fara a scrie nimic in consola!)")
-    # Rezultatele
     return {
         "user1": [user1, user_1_str, sum(user_1_int), scor_1],
         "user2": [user2, user_2_str, user_2_int, scor_2]
     }
 
-primul_joc = True
-if primul_joc == True:
-    rezultat = jocul(innregistrare, userii, scor_1, scor_2, scorul=False)
+userii = jocul(innregistrare, userii, scor_1, scor_2)
+print(userii)    
 
-# Calculare, afisarea scorului
-os.system('cls')
-print(logo)
-print("Cartile lui " + rezultat["user1"][0] + " sunt " + str(rezultat["user1"][1]) + " si a facut " + str(rezultat["user1"][2]) + ".")
-time.sleep(2)
-print("Cartile lui " + rezultat["user2"][0] + " sunt " + str(rezultat["user2"][1]) + " si a facut " + str(rezultat["user2"][2]) + ".")
-time.sleep(2)
-if rezultat["user1"][2] == rezultat["user2"][2]:
-    print("Remiza!")
-elif rezultat["user1"][2] == 21:
-    print("Felicitari " + rezultat["user1"][0] + " ai castigat partida!.")
-    rezultat["user1"][3] = rezultat["user1"][3] + 1
-elif rezultat["user2"][2] == 21:
-    print("Felicitari " + rezultat["user2"][0] + " ai castigat partida!")
-    rezultat["user2"][3] = rezultat["user2"][3] + 1
-elif rezultat["user1"][2] < 21 and rezultat["user2"][2] > 21:
-    print("Felicitari " + rezultat["user1"][0] + " ai castigat partida!.")
-    rezultat["user1"][3] = rezultat["user1"][3] + 1
-elif rezultat["user2"][2] < 21 and rezultat["user1"][2] > 21:
-    print("Felicitari " + rezultat["user2"][0] + " ai castigat partida!")
-    rezultat["user2"][3] = rezultat["user2"][3] + 1
-elif rezultat["user1"][2] < 21 and rezultat["user2"][2] < 21:
-    if (21 - rezultat["user1"][2]) < (21 - rezultat["user2"][2]):
-        print("Felicitari " + rezultat["user1"][0] + " ai castigat partida!.")
-        rezultat["user1"][3] = rezultat["user1"][3] + 1
-    else:
-        print("Felicitari " + rezultat["user2"][0] + " ai castigat partida!")
-        rezultat["user2"][3] = rezultat["user2"][3] + 1
-elif rezultat["user1"][2] > 21 and rezultat["user2"][2] > 21:
-    if (rezultat["user1"][2] - 21) < (rezultat["user2"][2] - 21):
-        print("Felicitari " + rezultat["user1"][0] + " ai castigat partida!.")
-        rezultat["user1"][3] = rezultat["user1"][3] + 1
-    else:
-        print("Felicitari " + rezultat["user2"][0] + " ai castigat partida!")
-        rezultat["user2"][3] = rezultat["user2"][3] + 1
-
-scorul = True
-if scorul == True:
-    print("Pana acum scorul este:")
-    print(rezultat["user1"][0] + ": " + str(rezultat["user1"][3]))
-    print(rezultat["user2"][0] + ": " + str(rezultat["user2"][3]))
-input("enter")
-os.system('cls')
-game0 = True
-while game0:
-    print("1. Vrei sa joci in continuare?. Scrie \"yes\" or \"y\" sau \"da\".")
-    print("2. Vrei sa iesi din joc?. Scrie \"no\" or \"n\" sau \"nu\".")
-    intrebare0 = input("Raspunde aici >>> ")
-    while intrebare0 != "yes" and intrebare0 != "y" and intrebare0 != "da" and intrebare0 != "YES" and intrebare0 != "Y" and intrebare0 != "DA" and intrebare0 != "no" and intrebare0 != "n" and intrebare0 != "nu" and intrebare0 != "NO" and intrebare0 != "N" and intrebare0 != "NU":
-        os.system('cls')
-        print("Nu am inteles....te rog reciteste ce am scris mai sus. Multumesc.")
-        intrebare0 = input("Raspunde aici >>> ")
-    if intrebare0 == "yes" or intrebare0 == "y" or intrebare0 == "da" or intrebare0 == "YES" or intrebare0 == "Y" or intrebare0 == "DA":
-        os.system('cls')
-        print("1. Scrie \"1\" daca doresti sa joci fara a reseta scorul si playerii innregistrati.")
-        print("2. Scrie \"2\" daca doresti sa stergi playerii si scorul innregistrat.")
-        print("3. Scrie \"3\" daca doresti doar scorul sa fie resetat.")
-        intrebare1 = input("Raspunde aici >>> ")
-        while intrebare1 != "1" and intrebare1 != "2" and intrebare1 != "3":
-            os.system('cls')
-            print("1. Scrie \"1\" daca doresti sa joci fara a reseta scorul si playerii innregistrati.")
-            print("2. Scrie \"2\" daca doresti sa stergi playerii si scorul innregistrat.")
-            print("3. Scrie \"3\" daca doresti doar scorul sa fie resetat.")
-            print("Nu am inteles. Te rog reciteste ce am scris mai sus. Multumesc.")
-            intrebare1 = input("Raspunde aici >>> ")
-        if intrebare1 == "1":
-            primul_joc = False
-            rezultat = jocul(innregistrare=False, userii=rezultat, scor_1=rezultat["user1"][2], scor_2=rezultat["user2"][2], scorul=True)
-        elif intrebare1 == "2":
-            primul_joc = False
-            innregistrare = True
-            userii = {}
-            scor_1 = 0
-            scor_2 = 0
-            rezultat = jocul(innregistrare, userii, scor_1, scor_2, scorul=True)
-        elif intrebare1 == "3":
-            primul_joc = False
-            innregistrare = False
-            scor_1 = 0
-            scor_2 = 0
-            rezultat = jocul(innregistrare, userii, scor_1, scor_2, scorul=True)
-    elif intrebare0 == "no" or intrebare0 == "n" or intrebare0 == "nu" or intrebare0 == "NO" or intrebare0 == "N" or intrebare0 == "NU":
-        os.system('cls')
-        print("Jocul s-a inchis!")
-        time.sleep(2)
     
+    
+
+
+# def castigator_remiza():
+#     if sum(user_1_int) < 21 and user_2_int > 21:
+#         scor_1 = scor_1 + 1
+#         os.system('cls')
+#         print(logo)
+#         print(userii["user_1"]) + f" cartile tale sunt {user_1_str} si ai in total {sum(user_1_int)}"
+#         time.sleep(2)
+#         print(userii["user_2"]) + f" cartile tale sunt {user_2_str} si ai in total {user_2_int}"
+#         time.sleep(2)
+#         print("Aceasta runda a castigat " + userii["user_1"])
+#         time.sleep(1)
+#         print("Scorul vostru:")
+#         time.sleep(1)
+#         print(userii["user_1"] + f" : {scor_1}")
+#         time.sleep(1)
+#         print(userii["user_2"] + f" : {scor_2}")
+#         time.sleep(1)
+#         input("Apasa \"ENTER\" pentru a continua")
+#     elif sum(user_1_int) > 21 and user_2_int < 21:
+#             scor_2 = scor_2 + 1
+#             os.system('cls')
+#             print(logo)
+#             print(userii["user_2"]) + f" cartile tale sunt {user_2_str} si ai in total {user_2_int}"
+#             time.sleep(2)
+#             print(userii["user_1"]) + f" cartile tale sunt {user_1_str} si ai in total {sum(user_1_int)}"
+#             time.sleep(2)
+#             print("Aceasta runda a castigat " + userii["user_2"])
+#             time.sleep(1)
+#             print("Scorul vostru:")
+#             time.sleep(1)
+#             print(userii["user_1"] + f" : {scor_1}")
+#             time.sleep(1)
+#             print(userii["user_2"] + f" : {scor_2}")
+#             time.sleep(1)
+#             input("Apasa \"ENTER\" pentru a continua")
+#     elif sum(user_1_int) < 21 and user_2_int < 21:
+#         if (21 - sum(user_1_int)) < (21 - user_2_int):
+#             scor_1 = scor_1 + 1
+#             os.system('cls')
+#             print(logo)
+#             print(userii["user_1"]) + f" cartile tale sunt {user_1_str} si ai in total {sum(user_1_int)}"
+#             time.sleep(2)
+#             print(userii["user_2"]) + f" cartile tale sunt {user_2_str} si ai in total {user_2_int}"
+#             time.sleep(2)
+#             print("Aceasta runda a castigat " + userii["user_1"])
+#             time.sleep(1)
+#             print("Scorul vostru:")
+#             time.sleep(1)
+#             print(userii["user_1"] + f" : {scor_1}")
+#             time.sleep(1)
+#             print(userii["user_2"] + f" : {scor_2}")
+#             time.sleep(1)
+#             input("Apasa \"ENTER\" pentru a continua")
+
+
+
+
+
+
+
+game0 = True
+# while game0:
+#     intrebare = input("Vrei sa joci in continujare?")
+#     if intrebare == "yes" or intrebare == "y":
+#         jocul(innregistrare=False, userii=userii, scor_1=scor_1, scor_2=scor_2)
